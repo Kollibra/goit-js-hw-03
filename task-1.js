31d6e5c6465d9965ff4bd88f8cf4613d8e12353c
Iryna Kovalova <100217476+Kollibra@users.noreply.github.com>
2023-10-27 11:16:53 +0300

    add task-1

--- task-1.js
+++ task-1.js
@@ -1,3 +1,14 @@
+function slugify(title) {
+    
+    title = title.toLowerCase();
+    
+    const words = title.split(' ');
+    
+    const slug = words.join('-');
+    return slug;
+  }
 
-
-   
+console.log(slugify("Arrays for begginers")); 
+console.log(slugify("English for developer")); 
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
